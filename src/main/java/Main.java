9be2b745c8d9ef510041cac8dd64cb1912026c87
jadekang <jkang.se@gmail.com>
2020-12-10 10:58:22 -0800

    Completed the basic Gradle practice.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,16 @@
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args) {
+        try {
+            CSVPrinter printer = new CSVPrinter(System.out, CSVFormat.EXCEL);
+            printer.printRecord("Jade", "Kang", 5, "Loved it!");
+            printer.printRecord("Taylor", "Rose", 3, "It was alright.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
